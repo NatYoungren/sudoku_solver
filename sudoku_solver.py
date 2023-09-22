ab65faa8d039276a5a181a99abed9ec53208dc28
NatYoungren <natahyoungren@gmail.com>
2023-09-22 16:23:20 -0400

    Fixed issues with propagate collapse

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -80,52 +80,61 @@ def prob_field_to_puzzle(prob_field: np.ndarray):
 @njit
 def propagate_collapse(prob_field: np.ndarray, collapsed_cells: np.ndarray):
     """ Collapses all cells with only one option until no more remain.
-        Returns the final state of the probability field.
-        Modifies collapsed_cells in place.
+        Modifies prob_field and collapsed_cells in place.
+        Returns a state code and the number of cells collapsed.
+
+    Args:
+        prob_field (np.ndarray): _description_
+        collapsed_cells (np.ndarray): _description_
+
+    Returns:
+        int, int: State code and number of cells collapsed.
+                    > State codes:  0 = invalid board state
+                                    1 = valid board state
+                                    2 = solved board state
     """
-    
     collapse_count = 0
-    prev_count = collapse_count
+    prev_count = -1
+    
     while True:
         # Sum the probability field along the value axis.
         resolution_map = prob_field.sum(axis=2)
         
         # If any cell has no options, the puzzle is unsolvable.
         if not resolution_map.all():
-            return None, collapse_count
+            break
 
         # If there is one choice per cell, return the solved probability field.
         if resolution_map.sum() == 81:
-            break
+            return 2, collapse_count
 
         # If the puzzle has been altered, look for new cells that can be collapsed.
         if prev_count != collapse_count: 
+            prev_count = collapse_count
+            
             # Iterate over all cells with only one option.
-            resolved_indices = np.argwhere(resolution_map == 1)
-            for x, y in resolved_indices:
+            resolvable_indices = np.argwhere(resolution_map == 1)
+            for x, y in resolvable_indices:
                 
                 # Skip cells that have been previously collapsed.
                 # FIXME: Rather than skipping these, we should avoid including them in resolved indices.
-                if collapsed_cells[x][y]:
+                if collapsed_cells[x, y]:
                     continue
                 
                 # Abort if the cell has no options.
-                if not prob_field[x][y].sum():
-                    return None, collapse_count
-                    
-                # Collapse the cell to the only option and propagate that change.
-                i = np.argmax(prob_field[x][y])
-                    
-                prob_field = collapse_probability_field(prob_field, x, y, i)
+                if not prob_field[x, y].sum():
+                    break
+                
+                # Collapse the cell to the only option and propagate that change.                    
+                collapse_probability_field(prob_field, x, y, np.argmax(prob_field[x][y]))
                 collapse_count += 1
 
                 # Mark the cell as collapsed.
                 collapsed_cells[x][y] = 1
         else:
-            break
-    
-    return prob_field, collapse_count
-
+            return 1, collapse_count
+        
+    return 0, collapse_count
 
 @njit
 def collapse_probability_field(prob_field: np.ndarray, x: int, y: int, i: int):
