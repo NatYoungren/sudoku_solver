5efe71c4e304dae330d1066b7d9c75884e61c204
NatYoungren <natahyoungren@gmail.com>
2023-09-25 12:54:51 -0400

    Updated heuristic maps to ignore collapsed cells

--- heuristic_utils.py
+++ heuristic_utils.py
@@ -12,27 +12,28 @@ import numpy as np
 #
 
 @njit
-def generate_heuristic_maps(prob_field: np.ndarray): # TODO: Consider returning the 3 sets of sums. (row, col, region)
+def generate_heuristic_maps(prob_field: np.ndarray, collapsed_cells: np.ndarray):
     """ Generates heuristics for each cell in the probability field.
 
     Args:
         prob_field (np.ndarray): 9x9x9 grid tracking the possibility of each cell containing each value.
+        collapsed_cells (np.ndarray): 9x9 grid tracking whether each cell has been collapsed.
+                                      Collapsed cells are left empty (0) in the heuristic maps.
 
     Returns:
-        collapse_map (np.ndarray), value_map (np.ndarray): 9x9x9 heuristic grids.
+        collapse_map (np.ndarray), weight_map (np.ndarray): 9x9x9 heuristic grids.
             > collapse_map: 9x9x9 grid tracking the minimum number of competing cells for that value in a row/column/region.
-            > weight_map: 9x9x9 grid tracking the number of cells affected any given collapse.
+            > weight_map: 9x9x9 grid tracking the maximum number of competing cells for that value in a row/column/region. # TODO: Consider changing to sum of all.
     """
-    
     row_sums, col_sums, region_sums = get_sums(prob_field)
     
-    # NOTE: Consider using prob_field.copy() multiplying the values.
-    #       This would leave all impossible values as 0.
     collapse_map = np.zeros((9, 9, 9), dtype=np.uint8)
     weight_map = np.zeros((9, 9, 9), dtype=np.uint8)
     
     for x in range(9):
         for y in range(9):
+            if collapsed_cells[x, y]:
+                continue
             for i in range(9):
                 # NOTE: Consider using sum?
                 weight_map[x, y, i] = max(col_sums[x, i], row_sums[y, i], region_sums[(x//3)*3+y//3, i])
@@ -64,32 +65,6 @@ def get_sums(prob_field: np.ndarray):
     
     return row_sums, col_sums, region_sums
 
-# @njit
-# def apply_collapse_scalar(prob_field: np.ndarray):
-#     # Modifies probability field to contain probability values between 0 and 1
-#     # Returns value field which contains the number of options
-    
-#     # Modifies the probability field in place.
-#     row_sums = prob_field.sum(axis=0)
-#     col_sums = prob_field.sum(axis=1)
-
-#     region_sums = np.zeros((9, 9))
-#     value_field = np.zeros((9, 9, 9))
-    
-#     # TODO: Make this more efficient?
-#     for x in range(3):
-#         for y in range(3):
-#             region_sums[x*3+y] = prob_field[x*3:x*3+3, y*3:y*3+3].sum(axis=0).sum(axis=0)
-
-#     for x in range(9):
-#         for y in range(9):
-#             for i in range(9):
-#                 # Consider using sum?
-#                 value_field[x, y, i] = max(col_sums[x, i], row_sums[y, i], region_sums[(x//3)*3+y//3, i])
-#                 prob_field[x, y, i] /= min(col_sums[x, i], row_sums[y, i], region_sums[(x//3)*3+y//3, i])
-                
-#     return value_field
-
 
 # Low collapse value means the choice is less likely to lead to a broken board state.
 @njit
@@ -99,9 +74,11 @@ def collapse_value(prob_field: np.ndarray, x: int, y: int, i: int):
     return min(prob_field[xx*3:xx*3+3, yy*3:yy*3+3, i].sum(), prob_field[x, :, i].sum(), prob_field[:, y, i].sum())
 
 
-# Note that the value of the cell overall included once in the final sum:
-#   row + col + (region - row/reg_overlap - col/reg_overlap) = sum
-#   cell + cell + (cell - cell - cell) = cell
+# NOTE: All methods below are unused.
+
+# Note that the value of x,y,i is counted once in the final sum:
+#   row + col + (region - (row ∩ reg) - (col ∩ reg) = sum
+#   cell + cell + (cell - cell - cell) = (cell) x 1
 @njit
 def collapse_sum(prob_field: np.ndarray, x: int, y: int, i: int):
     xx = x // 3
